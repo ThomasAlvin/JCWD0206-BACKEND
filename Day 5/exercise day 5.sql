fcd2e8fe9d681d54d92e2a4cb8c27a91bdf14b09
Jordan <jordansumardi@hotmail.com>
2023-05-09 16:55:37 +0700

    sql

--- Day 5/exercise day 5.sql
+++ Day 5/exercise day 5.sql
@@ -41,6 +41,8 @@ alter table students change id id int auto_increment , add primary key(id);
  -- select students
 select * from students;
 
+use sakila;
+select * from customer;
 
 use purwadhika_branch;
 -- 9 
@@ -82,7 +84,23 @@ where district in ('California' ,'Alberta' ,'Mekka');
 
 select count(last_name) from actor where last_name like '%WOOD%';
 
+use sakila;
 select customer_id, sum(amount) from payment group by customer_id having sum(amount) > 20;
+
+
+select first_name, last_name, sum(amount) from payment p 
+join customer c on c.customer_id = p.customer_id
+group by first_name,last_name having sum(amount) > 20;
+
+
+select concat(first_name, " ",last_name) as fullname, sum(amount) from payment p 
+join customer c on c.customer_id = p.customer_id
+group by fullname having sum(amount) > 20;
+
+
+select customer_id, sum(amount) from payment where  sum(amount) > 20 group by customer_id; 
+-- ga bisa
+
 
 select customer_id, sum(amount),payment_id from payment 
 group by amount, customer_id, payment_id order by sum(amount) desc limit 3;
@@ -97,12 +115,16 @@ from payment group by customer_id order by count(customer_id) desc, sum(amount) 
 select * from actor;
 insert into actor (first_name,last_name,last_update) values ('JHONNY','DAVIS',CURDATE());
 
-
 insert into actor (first_name,last_name,last_update) values ('ADAM','DAVIS',CURDATE()),
 ('JEREMY','DAVIS',CURDATE()), ('CRAIG','DAVIS',CURDATE()),  ('STEVE','DAVIS',CURDATE())
 ;
 
 select count(last_name),last_name from actor  where last_name = 'DAVIS';
+
+select * from actor limit 1;
+select * from film_actor where actor_id = 1;
+delete from actor where actor_id = 1;
+delete from film_actor where actor_id = 1;
 
 delete from film_actor where actor_id = 4;
 delete from actor where last_name = 'DAVIS' and first_name = 'JENNIFER';
@@ -110,9 +132,85 @@ delete from actor where last_name = 'DAVIS' and first_name = 'JENNIFER';
 
 UPDATE actor set first_name = 'GEORGE' where last_name = 'Davis';
 select count(actor_id), actor_id from film_actor group by actor_id order by count(actor_id) desc limit 10;
+select count(actor_id), actor_id from film_actor group by actor_id order by count(actor_id) desc limit 10;
+
+-- subquery 
+
+select * from actor;
+-- sumber data => memiliki return lebih dari 1 record/row
+-- kolom => memiliki satu return record/row  saja dan return 1 kolom saja 
+
+-- select * from something = table, query.
+
+select * from (select count(actor_id), concat(first_name, " ", last_name)  from actor) this;
+
+select total,full_name from
+(
+select count(actor_id) total, concat(first_name, " ", last_name) full_name  from actor group by first_name,last_name
+) this;
+
+select count(actor_id), last_name from actor group by last_name;
+
+select count(actor_id) from actor where last_name = "AKROYD";
+
+select distinct (select count(actor_id) from actor where last_name = a.last_name) 
+as total, last_name from actor as a;
 
 
+select count(amount) freq_payment, 
+sum(amount) total_payment, customer_id from payment group by customer_id order by count(amount) desc limit 3;
 
+select * from
+(
+select count(amount) freq_payment, 
+sum(amount) total_payment, customer_id from payment group by customer_id order by count(amount) desc limit 3
+) this order by this.total_payment desc;
+
+
+-- Display title, description, length, and rating from film,
+-- where special features include deleted scenes and behind the scenes order by most length
+
+select * from film;
+select title,description,length,rating from film where special_features like '%Deleted Scenes,Behind the Scenes%'
+order by length desc;
+
+-- Display country and total of inactive customer (active = 0)
+-- from country where customer active = 0 order by the highest inactive (active = 0) customer
+
+
+select * from customer; -- active = 0 , addressid
+select * from address; -- cityid
+select * from city; -- country id
+select * from country; -- country
+
+use sakila;
+-- count(active)
+select c.first_name,c.last_name, a.address, ct.city , cy.country from customer c 
+join address a on a.address_id = c.address_id
+join city ct on ct.city_id = a.city_id
+join country cy on cy.country_id = ct.country_id
+where c.active = 0;
+
+select count(c.active) inactive_customer, cy.country from customer c 
+join address a on a.address_id = c.address_id
+join city ct on ct.city_id = a.city_id 
+join country cy on cy.country_id = ct.country_id
+where c.active = 0
+group by cy.country order by count(c.active) desc;
+
+select * from address;
+select * from customer;
+
+select * from address a
+join customer c on c.address_id = a.address_id;
+
+-- students id,firstname,lastname,age,email,gender, programId, locationId
+-- 1, ucok, baba , 17, u@mail.com,male, 1, 2
+-- 2, udin, baba , 17, u@mail.com,male, 1, 1
+-- 100 data memiliki program dan location yang sama
+-- programs id,program,location
+-- 1 , 'JCWD', location ENUM(BSD,JKT,BATAM,BANDUNG), pic
+-- 
 
 -- world
 
@@ -121,15 +219,34 @@ use world;
 select name,population from country order by population desc limit 1;
 
 select name,population from country order by population desc limit 1 offset 1;
+select name,population from country order by population desc limit 1,1;
 
-select name,population from country order by population  limit 1;
+
+select name,population from country order by population,name limit 2,1 ;
+select name,population from country order by population limit 1 offset 1;
+
 
 select name,population from country where population = (select min(population) from country) limit 1 offset 2;
 
 select sum(surfaceArea), continent from country where lifeExpectancy > 75 group by continent order by sum(surfacearea) desc limit 1;
 
 
+use world;
 
+select sum(surfaceArea) surface, continent from country 
+where lifeExpectancy > 75 group by continent order by sum(surfaceArea) desc;
+
+
+select sum(surfaceArea) surface, continent,lifeExpectancy 
+from country group by continent,lifeExpectancy 
+having lifeExpectancy > 75 order by sum(surfaceArea) desc;
+
+select sum(surface), continent from (
+
+select sum(surfaceArea) surface, continent,lifeExpectancy 
+from country group by continent,lifeExpectancy 
+having lifeExpectancy > 75 order by sum(surfaceArea) desc
+) this group by this.continent;
 
 
 
---
+++ Day 6/database.sql
@@ -0,0 +1,94 @@
+-- day 6
+
+-- database 
+-- database yang baik : 
+					-- tidak redundant,
+					-- memiliki access key untuk mengkases informasi antar table,
+                    -- informasi sesuai 
+                    -- informasi mudah diakses karena miliki design database yang terstruktur 
+                    
+-- relational database : 
+					-- one to one . hubungan antar table dihubungkan dari masing2 primary key
+                    -- one to many. hubungan antara table dihubungkan dengan primary key dan foreign key . 
+-- country 
+-- id , nama 
+1 , indonesia 
+2, amerika
+
+-- customer 
+-- id , nama, countryId
+1, chris, 1
+2, fahmi, 1 
+3, mikhael, 2
+                    -- many to many. hubungan antara table dihubungkan dengan primary key dan foreign key .
+-- students 
+-- id, nama 
+1, chris 
+2, baraak 
+3, ridho
+
+-- programs
+-- id , nama_program
+1, masak bareng master chef
+2, belajar javascript 
+3, belajar ui/ux
+4, belajar data science 
+
+-- students_programs
+-- student_id, program_id
+1,1 
+1, 2 
+2, 1 
+1,1 => error 
+composite key merupakan gabungan antara student_id dan program_id
+
+-- db_purwadhika 
+-- create 3 tables. students , programs , branch 
+-- id, name, program, branch
+1. udin, JCWD, batam 
+2. rey, JCWD, batam 
+3. fahmi, JCWD,batam 
+
+
+-- relational database 
+-- primary key, foreign key, composite key
+
+-- join, left join , right join 
+join = mencari persamaan antara dua table 
+left join = mencari persamaan antara dua table, 
+			data yg tidak memiliki persamaan akan tetap muncul selama data tersebut milik si parent. 
+right join =  mencari persamaan antara dua table, 
+			data yg tidak memiliki persamaan akan tetap muncul selama data tersebut milik si children. 
+            
+-- contoh            
+  
+select c.customer_id, c.first_name, ad.address, ad.address_id from customer c
+right join address ad on ad.address_id = c.address_id; -- parent = customer , child = address
+-- semua data address akan muncul sebagai child
+
+select c.customer_id, c.first_name, ad.address, ad.address_id from address ad 
+left join customer c on c.address_id = ad.address_id; -- parent = address, child = customer
+-- semua data address akan muncul sebagai parent
+
+select c.customer_id, c.first_name, ad.address, ad.address_id from address ad 
+join customer c on c.address_id = ad.address_id; 
+-- data yang muncul hanyalah data yg memiliki persamaan  
+   
+   use sakila;
+-- subquery 
+		-- di dalam kolom 
+				select  (select first_name from customer where customer_id = payment.customer_id) test , payment.* from payment;
+                
+        -- di dalam from 
+        -- di dalam where
+        -- didalam having 
+        -- dalam join
+        
+        
+
+-- union 
+
+-- transaction 
+
+-- index 
+
